3468ab33aecaa60eb659519fe92c104d195a1602
nabi <sandurovnabi98@gmail.com>
2025-11-03 10:35:02 +0600

    adde change HTTPTransport.ts

--- src/services/HTTPTransport.ts
+++ src/services/HTTPTransport.ts
@@ -1,3 +1,97 @@
+// enum METHODS {
+//   GET = 'GET',
+//   POST = 'POST',
+//   PUT = 'PUT',
+//   DELETE = 'DELETE',
+// }
+//
+// type Options = {
+//   method: METHODS;
+//   data?: Record<string, unknown>;
+//   headers?: Record<string, string>;
+//   timeout?: number;
+// }
+//
+// type OptionsWithoutMethod = Omit<Options, 'method'>;
+//
+// export default class HTTPTransport {
+//   static API_URL = 'https://ya-praktikum.tech/api/v2';
+//   protected endpoint: string;
+//
+//   constructor(endpoint: string) {
+//     this.endpoint = `${HTTPTransport.API_URL}${endpoint}`;
+//   }
+//
+//   public get = (url: string, options: OptionsWithoutMethod = {}): Promise<XMLHttpRequest> => {
+//     return this.request(url, { ...options, method: METHODS.GET }, options.timeout);
+//   };
+//
+//   public post = (url: string, options: OptionsWithoutMethod = {}): Promise<XMLHttpRequest> => {
+//     return this.request(url, { ...options, method: METHODS.POST }, options.timeout);
+//   };
+//
+//   public put = (url: string, options: OptionsWithoutMethod = {}): Promise<XMLHttpRequest> => {
+//     return this.request(url, { ...options, method: METHODS.PUT }, options.timeout);
+//   };
+//
+//   public delete = (url: string, options: OptionsWithoutMethod = {}): Promise<XMLHttpRequest> => {
+//     return this.request(url, { ...options, method: METHODS.DELETE }, options.timeout);
+//   };
+//
+//   private request = (url: string, options: Options, timeout = 5000): Promise<XMLHttpRequest> => {
+//     const { headers = {}, method, data } = options;
+//
+//     return new Promise((resolve, reject) => {
+//       if (!method) {
+//         reject(new Error('No method'));
+//         return;
+//       }
+//
+//       const xhr = new XMLHttpRequest();
+//       const isGet = method === METHODS.GET;
+//
+//       xhr.open(method, isGet && !!data ? `${this.endpoint}${url}${this.queryStringify(data)}` : `${this.endpoint}${url}`);
+//
+//       Object.keys(headers).forEach(key => {
+//         xhr.setRequestHeader(key, headers[key]);
+//       });
+//
+//       xhr.onload = () => {
+//         resolve(xhr);
+//       };
+//
+//       xhr.onabort = reject;
+//       xhr.onerror = reject;
+//
+//       xhr.timeout = timeout;
+//       xhr.ontimeout = reject;
+//
+//       xhr.withCredentials = true;
+//
+//       if (isGet || !data) {
+//         xhr.send();
+//       } else if (data instanceof FormData) {
+//         xhr.send(data);
+//       } else {
+//         xhr.setRequestHeader('Content-Type', 'application/json');
+//         xhr.send(JSON.stringify(data));
+//       }
+//     });
+//   };
+//
+//   private queryStringify(data: Record<string, unknown>): string {
+//     if (typeof data !== 'object' || data === null) {
+//       throw new Error('Data must be object');
+//     }
+//
+//     const keys = Object.keys(data);
+//     return keys.reduce((result, key, index) => {
+//       return `${result}${key}=${data[key]}${index < keys.length - 1 ? '&' : ''}`;
+//     }, '?');
+//   }
+// }
+
+
 enum METHODS {
   GET = 'GET',
   POST = 'POST',
@@ -7,7 +101,7 @@ enum METHODS {
 
 type Options = {
   method: METHODS;
-  data?: Record<string, unknown>;
+  data?: any; // теперь любой тип данных
   headers?: Record<string, string>;
   timeout?: number;
 }
@@ -50,19 +144,17 @@ export default class HTTPTransport {
       const xhr = new XMLHttpRequest();
       const isGet = method === METHODS.GET;
 
-      xhr.open(method, isGet && !!data ? `${this.endpoint}${url}${this.queryStringify(data)}` : `${this.endpoint}${url}`);
+      const finalUrl = isGet && data ? `${this.endpoint}${url}${this.queryStringify(data)}` : `${this.endpoint}${url}`;
+      xhr.open(method, finalUrl);
 
+      // Заголовки
       Object.keys(headers).forEach(key => {
         xhr.setRequestHeader(key, headers[key]);
       });
 
-      xhr.onload = () => {
-        resolve(xhr);
-      };
-
+      xhr.onload = () => resolve(xhr);
       xhr.onabort = reject;
       xhr.onerror = reject;
-
       xhr.timeout = timeout;
       xhr.ontimeout = reject;
 
@@ -71,22 +163,27 @@ export default class HTTPTransport {
       if (isGet || !data) {
         xhr.send();
       } else if (data instanceof FormData) {
+        // FormData отправляем без Content-Type
         xhr.send(data);
-      } else {
+      } else if (typeof data === 'object') {
         xhr.setRequestHeader('Content-Type', 'application/json');
         xhr.send(JSON.stringify(data));
+      } else {
+        // Для простых типов (string, number)
+        xhr.send(data);
       }
     });
   };
 
-  private queryStringify(data: Record<string, unknown>): string {
+  private queryStringify(data: Record<string, any>): string {
     if (typeof data !== 'object' || data === null) {
-      throw new Error('Data must be object');
+      throw new Error('Data must be an object');
     }
 
-    const keys = Object.keys(data);
-    return keys.reduce((result, key, index) => {
-      return `${result}${key}=${data[key]}${index < keys.length - 1 ? '&' : ''}`;
-    }, '?');
+    return Object.keys(data)
+      .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`)
+      .join('&')
+      ? `?${Object.keys(data).map(key => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`).join('&')}`
+      : '';
   }
 }
