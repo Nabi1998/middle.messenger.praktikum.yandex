e873754d9e7577c263a15f999ad55ce875b28119
nabi <sandurovnabi98@gmail.com>
2025-11-03 10:58:56 +0600

    change interface

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -8,6 +8,7 @@ export interface User {
   email: string;
   phone: string;
   avatar?: string;
+  display_name?: string;
 }
 
 export interface LoginData {
--- src/types/api.ts
+++ src/types/api.ts
@@ -17,11 +17,12 @@ export interface User {
   id: number;
   first_name: string;
   second_name: string;
-  display_name: string;
+  display_name?: string;
   login: string;
   email: string;
   phone: string;
   avatar: string;
+
 }
 
 // User profile types
